afad16d36cb40daa55146094901fa67d896d5d2e
SemenovaAA04 <asy.sem13@gmail.com>
2025-05-23 06:45:11 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,24 @@
+import sqlite3
+
+conn = sqlite3.connect("trackers.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS trackers (
+    user_id TEXT,
+    name TEXT
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS logs (
+    user_id TEXT,
+    name TEXT,
+    minutes INTEGER,
+    date TEXT
+)
+""")
+
+conn.commit()
+conn.close()
 
